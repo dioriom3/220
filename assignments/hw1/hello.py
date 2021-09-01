baee5dae4e0cb8fa26ecfa9328ef471a8108bab6
Michael Diorio <dioriom@g.cofc.edu>
2021-08-31 20:12:27 -0400

    220-HW1-Completed

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,12 @@
+"""
+Name: Michael Diorio
+File Name: hello.py
+
+Problem: To print a simple statement
+
+Certification of Authenticity: I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
