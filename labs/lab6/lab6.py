5d04201d30bf0881aa91efe89c60586e5e241b93
Michael Diorio <dioriom@g.cofc.edu>
2021-10-07 15:01:17 -0400

    completed lab6

--- labs/lab6/lab6.py
+++ labs/lab6/lab6.py
@@ -1,18 +1,69 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Michael Diorio
+lab6.py
 """
 
 
 def name_reverse():
-    """
-    Read a name in first-last order and display it in last-comma-first order.
-    """
+    s = input("Enter a first name then last name: ")
+    s = s.split()
+    print(s[1] + ", " + s[0])
+
+
+def company_name():
+    x = input("Enter a three-part internet domain name: ")
+    x = x.split(".")
+    print(x[1])
+
+
+def initials():
+    name = eval(input("How many names would you like to input? : "))
+    for i in range(0, name):
+        first_name = input("Enter a first name: ")
+        last_name = input("Enter the last name: ")
+        print("The initials are: ", first_name[0] + last_name[0])
+
+
+def names():
+    y = input("Enter a list of names separated by a comma: ")
+    y = y.split(", ")
+    for i in y:
+        parts = i.split()
+        print("The initials of the names are: ", parts[0][0] + parts[1][0])
+
+
+def thirds():
+    n = eval(input("How many sentences would you like to input? : "))
+    for i in range(n):
+        s = input("Enter a sentence: ")
+        print(s[2:-1:3])
+
+
+def word_average():
+    x = input("Enter a sentence: ")
+    acc = 0
+    x = x.split()
+    for word in x:
+        acc = acc + len(word)
+    print(acc / len(x))
+
+
+def pig_latin():
+    x = input("Enter a sentence: ")
+    x = x.lower()
+    x = x.split()
+    for word in x:
+        print(word[1:] + word[0] + "ay")
 
 
 def main():
-    name_reverse()
-    # add other function calls here
+    # name_reverse()
+    # company_name()
+    # initials()
+    # names()
+    # thirds()
+    # word_average()
+    # pig_latin()
 
 
 main()
